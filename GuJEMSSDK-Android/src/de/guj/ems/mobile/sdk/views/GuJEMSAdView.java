3ffe947fe3c68f03789b2eecaa936355d9c5667f
GUJEMS@googlemail.com <GUJEMS@googlemail.com@8f5c32ab-0f87-1bfd-a350-b0955cbaf156>
2012-11-30 16:38:12 +0000

    - Fixed possible ANR with asynchronous ad requests - Introduced callback interface for ad response handlers - Introduced reload method for AdViews

--- GuJEMSSDK-Android/src/de/guj/ems/mobile/sdk/views/GuJEMSAdView.java
+++ GuJEMSSDK-Android/src/de/guj/ems/mobile/sdk/views/GuJEMSAdView.java
@@ -14,6 +14,7 @@ import android.content.res.XmlResourceParser;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Xml;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import de.guj.ems.mobile.sdk.controllers.AdServerAccess;
 import de.guj.ems.mobile.sdk.controllers.AmobeeSettingsAdapter;
@@ -27,28 +28,30 @@ import de.guj.ems.mobile.sdk.util.UserAgentHelper;
  * The webview uses as container to display an ad. Derived from the ORMMA
  * refernce implementaton of an ad view container.
  * 
- * This class adds folowing capabilites to the reference implementation:
- * - loading data with an asynchronous HTTP request
- * - initializing the view from XML by passing a resource ID
- * - adding custom view-specific parameters to a placement's ad request (runtime)
- * - adding matching or non-matching keywords to a placement's ad request (runtime)
- * - adding the javascript interface EMSInterface to the view
+ * This class adds folowing capabilites to the reference implementation: -
+ * loading data with an asynchronous HTTP request - initializing the view from
+ * XML by passing a resource ID - adding custom view-specific parameters to a
+ * placement's ad request (runtime) - adding matching or non-matching keywords
+ * to a placement's ad request (runtime) - adding the javascript interface
+ * EMSInterface to the view
  * 
- * ONLY USE THIS CLASS IF YOU WANT TO ADD THE VIEW PROGRAMMATICALLY INSTEAD
- * OF DEFINING IT WITHIN A LAYOUT.XML FILE!
+ * ONLY USE THIS CLASS IF YOU WANT TO ADD THE VIEW PROGRAMMATICALLY INSTEAD OF
+ * DEFINING IT WITHIN A LAYOUT.XML FILE!
  * 
  * @author stein16
- *
+ * 
  */
-public class GuJEMSAdView extends OrmmaView implements OnGlobalLayoutListener {
+public class GuJEMSAdView extends OrmmaView implements OnGlobalLayoutListener, AdResponseHandler {
 
 	private IAdServerSettingsAdapter settings;
 
 	private final String TAG = "GuJEMSAdView";
 
 	/**
-	 * Initialize view without configuration 
-	 * @param context android application context
+	 * Initialize view without configuration
+	 * 
+	 * @param context
+	 *            android application context
 	 */
 	public GuJEMSAdView(Context context) {
 		super(context);
@@ -57,8 +60,11 @@ public class GuJEMSAdView extends OrmmaView implements OnGlobalLayoutListener {
 
 	/**
 	 * Initialize view with attribute set (this is the common constructor)
-	 * @param context android application context
-	 * @param resId resource ID of the XML layout file to inflate from
+	 * 
+	 * @param context
+	 *            android application context
+	 * @param resId
+	 *            resource ID of the XML layout file to inflate from
 	 */
 	public GuJEMSAdView(Context context, AttributeSet set) {
 		super(context, set);
@@ -68,8 +74,11 @@ public class GuJEMSAdView extends OrmmaView implements OnGlobalLayoutListener {
 
 	/**
 	 * Initialize view from XML
-	 * @param context android application context
-	 * @param resId resource ID of the XML layout file to inflate from
+	 * 
+	 * @param context
+	 *            android application context
+	 * @param resId
+	 *            resource ID of the XML layout file to inflate from
 	 */
 	public GuJEMSAdView(Context context, int resId) {
 		super(context);
@@ -79,9 +88,13 @@ public class GuJEMSAdView extends OrmmaView implements OnGlobalLayoutListener {
 
 	/**
 	 * Initialize view from XML and add any custom parameters to the request
-	 * @param context android application context
-	 * @param customParams map of custom param names and thiur values
-	 * @param resId resource ID of the XML layout file to inflate from
+	 * 
+	 * @param context
+	 *            android application context
+	 * @param customParams
+	 *            map of custom param names and thiur values
+	 * @param resId
+	 *            resource ID of the XML layout file to inflate from
 	 */
 	public GuJEMSAdView(Context context, Map<String, ?> customParams, int resId) {
 		super(context);
@@ -93,11 +106,17 @@ public class GuJEMSAdView extends OrmmaView implements OnGlobalLayoutListener {
 	/**
 	 * Initialize view from XML and add matching or non-matching keywords as
 	 * well as any custom parameters to the request
-	 * @param context android application context
-	 * @param customParams map of custom param names and thiur values
-	 * @param kws matching keywords
-	 * @param nkws non-matching keywords
-	 * @param resId resource ID of the XML layout file to inflate from
+	 * 
+	 * @param context
+	 *            android application context
+	 * @param customParams
+	 *            map of custom param names and thiur values
+	 * @param kws
+	 *            matching keywords
+	 * @param nkws
+	 *            non-matching keywords
+	 * @param resId
+	 *            resource ID of the XML layout file to inflate from
 	 */
 	public GuJEMSAdView(Context context, Map<String, ?> customParams,
 			String[] kws, String nkws[], int resId) {
@@ -109,10 +128,15 @@ public class GuJEMSAdView extends OrmmaView implements OnGlobalLayoutListener {
 
 	/**
 	 * Initialize view from XML and add matching or non-matching keywords
-	 * @param context android application context
-	 * @param kws matching keywords
-	 * @param nkws non-matching keywords
-	 * @param resId resource ID of the XML layout file to inflate from
+	 * 
+	 * @param context
+	 *            android application context
+	 * @param kws
+	 *            matching keywords
+	 * @param nkws
+	 *            non-matching keywords
+	 * @param resId
+	 *            resource ID of the XML layout file to inflate from
 	 */
 	public GuJEMSAdView(Context context, String[] kws, String nkws[], int resId) {
 		super(context);
@@ -168,27 +192,21 @@ public class GuJEMSAdView extends OrmmaView implements OnGlobalLayoutListener {
 		return as;
 	}
 
-	private final void load() {
-
+	@Override
+	public void reload() {
 		if (settings != null) {
+			super.clearView();
 
 			// Construct request URL
 			final String url = this.settings.getRequestUrl();
 			// Run off main UI thread if Android > 4.0
 			if (Connectivity.isOnline() && Build.VERSION.SDK_INT > 10) {
-				
+
 				SdkLog.i(TAG, "ems_adcall: START async. AdServer request");
 				SdkLog.d(TAG, "ems_adcall: url = " + url);
-				AdServerAccess mAdFetcher = (AdServerAccess) (new AdServerAccess(
-						UserAgentHelper.getUserAgent()))
+				new AdServerAccess(
+						UserAgentHelper.getUserAgent(), this)
 						.execute(new String[] { url });
-				try {
-					loadData(mAdFetcher.get(), "text/html", "utf-8");
-					setTimeoutRunnable(new TimeOutRunnable());					
-				} catch (Exception e) {
-					SdkLog.e(TAG, "Error loading ads...", e);
-				}
-				SdkLog.i(TAG, "ems_adcall: FINISH async. AdServer request");
 			}
 			// default behaviour Android < 4.0
 			else if (Connectivity.isOnline()) {
@@ -202,7 +220,45 @@ public class GuJEMSAdView extends OrmmaView implements OnGlobalLayoutListener {
 		}
 	}
 
+	private final void load() {
+
+		if (settings != null) {
+
+			// Construct request URL
+			final String url = this.settings.getRequestUrl();
+			// Run off main UI thread if Android > 4.0
+			if (Connectivity.isOnline() && Build.VERSION.SDK_INT > 10) {
+
+				SdkLog.i(TAG, "ems_adcall: START async. AdServer request");
+				SdkLog.d(TAG, "ems_adcall: url = " + url);
+				new AdServerAccess(
+						UserAgentHelper.getUserAgent(), this)
+						.execute(new String[] { url });
+			}
+			// default behaviour Android < 4.0
+			else if (Connectivity.isOnline()) {
+				this.loadUrl(url);
+			}
+			// Do nothing if offline
+			else if (Connectivity.isOffline()) {
+				SdkLog.i(TAG,
+						"ems_adcall: No network connection - not requesting ads.");
+			}
+		}
+	}
+	
+	public final void processResponse(String response) {
+		try {
+			setTimeoutRunnable(new TimeOutRunnable());
+			loadData(response, "text/html", "utf-8");
+			SdkLog.i(TAG, "ems_adcall: FINISH async. AdServer request");
+		} catch (Exception e) {
+			SdkLog.e(TAG, "Error loading ads...", e);
+		}
+	}
+
 	private void preLoadInitialize(Context context, AttributeSet set) {
+		setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 		this.addJavascriptInterface(new EMSInterface(), "emsmobile");
 		if (set != null) {
 			this.settings = new AmobeeSettingsAdapter(context, set);
@@ -211,6 +267,7 @@ public class GuJEMSAdView extends OrmmaView implements OnGlobalLayoutListener {
 
 	private void preLoadInitialize(Context context, AttributeSet set,
 			String[] kws, String[] nkws) {
+		setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 		this.addJavascriptInterface(new EMSInterface(), "emsmobile");
 		if (set != null) {
 			this.settings = new AmobeeSettingsAdapter(context, set, kws, nkws);
