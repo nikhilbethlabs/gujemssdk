01aa47dc41e923933b3570e763b39fe0b0719d1a
arne.steinmetz@gmail.com <arne.steinmetz@gmail.com@8f5c32ab-0f87-1bfd-a350-b0955cbaf156>
2013-05-07 11:27:38 +0000

    Set VersionHelper to 1.2.5

--- GuJEMSSDK-Android/src/de/guj/ems/mobile/sdk/util/VersionHelper.java
+++ GuJEMSSDK-Android/src/de/guj/ems/mobile/sdk/util/VersionHelper.java
@@ -6,7 +6,7 @@ public class VersionHelper {
 	
 	public final static int MINOR_VERSION = 2;
 	
-	public final static int REV_VERSION = 4;
+	public final static int REV_VERSION = 5;
 	
 	public final static String VERSION_STR = MAJOR_VERSION + "_" + MINOR_VERSION + "_" + REV_VERSION;
 	
